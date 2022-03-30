aaefd2018578ffb686e84a8481526a7bab2a7b7c
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-03-30 19:55:59 +0200

    Changed nvim-cmp bindings

--- nvim/.config/nvim/lua/lsp/lsp.lua
+++ nvim/.config/nvim/lua/lsp/lsp.lua
@@ -1,37 +1,47 @@
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
+local function complete_next()
+    if cmp.visible() then
+        cmp.select_next_item()
+    else
+        cmp.complete()
+    end
+end
+
+local function complete_prev(fallback)
+    if cmp.visible() then
+        cmp.select_prev_item()
+    else
+        fallback()
+    end
+end
+
 cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      end,
+        expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
     },
     mapping = {
-      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-      ['<C-e>'] = cmp.mapping({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-n>'] = cmp.mapping(complete_next(), { 'i', 'c' }),
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     },
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      --{ name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-       { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
-      { name = 'buffer' },
-    })
-  })
+        sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'ultisnips' }, -- For ultisnips users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
 
   -- Set configuration for specific filetype.
   cmp.setup.filetype('gitcommit', {
@@ -53,9 +63,9 @@ cmp.setup({
   cmp.setup.cmdline(':', {
     sources = cmp.config.sources({
       { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
+    }
+    --{ { name = 'cmdline' } }
+    )
   })
 
   -- Setup lspconfig.
