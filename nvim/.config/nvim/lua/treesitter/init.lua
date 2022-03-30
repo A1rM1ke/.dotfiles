7bc444d3917d372a8716c22e1ec363811d1dfa7b
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-03-30 19:43:14 +0200

    Tmux changed escape timeout time, and added latex to the treesitter

--- nvim/.config/nvim/lua/treesitter/init.lua
+++ nvim/.config/nvim/lua/treesitter/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {"c", "python"},
+  ensure_installed = {"c", "python", "latex"},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
