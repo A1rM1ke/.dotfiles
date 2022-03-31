6017dd18f512129d6de8c7820920fbd9e2ecfa31
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-03-31 19:55:05 +0200

    Added more treesitter parsers HTML, CSS, JS, Lua parsers added to the Treesitter ensure install list

--- nvim/.config/nvim/lua/treesitter/init.lua
+++ nvim/.config/nvim/lua/treesitter/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {"c", "python", "latex"},
+  ensure_installed = {"c", "python", "latex", "html", "css", "javascript", "lua"},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
