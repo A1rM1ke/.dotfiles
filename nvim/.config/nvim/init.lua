a8ffe4fd7feb33ff76e233c6a96333828fced77c
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-07-24 15:49:18 +0400

    Updated init.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,14 @@
+require 'renzum.plugins'
+require 'renzum.mappings'
+require 'renzum.settings'
+require 'renzum.autocmd'
+
+require 'renzum.plugins.gruvbox'
+require 'renzum.plugins.airline'
+
+
+require('lsp')
+require('treesitter')
+require('ts-context')
+require('my-rust-tools')
+require('lsp_lines').setup()
