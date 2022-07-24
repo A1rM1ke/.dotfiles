823767a00de7b5b0b840e4f5ef8988a73e2480a6
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-07-24 15:22:05 +0400

    Converted settings to lua

---
+++ nvim/.config/nvim/lua/renzum/settings.lua
@@ -0,0 +1,28 @@
+local set = vim.opt
+
+set.relativenumber = true
+set.number = true
+set.compatible = false
+set.timeoutlen = 200
+set.scrolloff = 8
+set.wildmenu = true
+set.errorbells = false
+set.title = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.incsearch = true
+set.linebreak = true
+set.cursorline = true
+set.background = 'dark'
+set.showcmd = true
+set.showmode = false
+set.autoread = true
+set.splitbelow = true
+set.updatetime = 100
+
+vim.options = {
+    colorcolumn = '80,120'
+}
+
+vim.cmd('syntax on')
