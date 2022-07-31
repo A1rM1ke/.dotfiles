bb562ad8db85ab9a858c1d4c42c49951576e7fea
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-07-31 17:50:31 +0400

    Got luasnip working

--- nvim/snippets/all.lua
+++ nvim/snippets/all.lua
@@ -2,10 +2,34 @@ local ls = require('luasnip')
 
 local s = ls.s
 local t = ls.t
+local i = ls.i
 
-local testsnippet = s("testsinppet", {
-    t("Test Snippet Inserted")
+local d = ls.dynamic_node
+local c = ls.choice_node
+local f = ls.function_noed
+local sn = ls.snippet_node
+local rep = require('luasnip.extras').rep
+
+local fmt = require('luasnip.extras.fmt').fmt
+
+local snippets, autosnippets = {}, {}
+--[[
+
+local testsnippet = s("testsnippet", {
+    t("Test Snippet Inserted"),
+    i(1, "placeholder_text"),
 })
 
-return {testsnippet}
+local secondsnippet = s("secondsnippet",
+    fmt(--[[
+    local {} = function({})
+        {}
+    end
+    -]-], {i(1, "myVar"), i(2, "myArg"), i(3, "--TODO: Something")})
+)
+]]
 
+table.insert(snippet_table, testsnippet)
+table.insert(snippet_table, secondsnippet)
+
+return snippet_table
